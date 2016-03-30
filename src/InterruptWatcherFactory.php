1e03a354f2515cc08661e642d5e59b223541b0b4
Andrew Carter <andrewcarter1992@gmail.com>
2016-03-30 12:36:16 +0100

    Update InterruptWatcherFactory.php

--- src/InterruptWatcherFactory.php
+++ src/InterruptWatcherFactory.php
@@ -4,6 +4,9 @@ namespace PiPHP\GPIO;
 
 final class InterruptWatcherFactory implements InterruptWatcherFactoryInterface
 {
+    /**
+     * {@inheritdoc}
+     */
     public function createWatcher()
     {
         return new InterruptWatcher();
