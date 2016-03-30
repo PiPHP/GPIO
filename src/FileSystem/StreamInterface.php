edc4fe39bad7496438d36730d889557d37eec0a3
Andrew Carter <andrewcarter1992@gmail.com>
2016-03-30 23:36:18 +0100

    Added select() to StreamInterface

--- src/FileSystem/StreamInterface.php
+++ src/FileSystem/StreamInterface.php
@@ -5,6 +5,19 @@ namespace PiPHP\GPIO\FileSystem;
 interface StreamInterface
 {
     /**
+     * Performs the equivalent of a select() system call on the streams provided.
+     * 
+     * @param self[] &$read
+     * @param self[] &$write
+     * @param self[] &$except
+     * @param int     $tvSec
+     * @param int     $tvUSec
+     *
+     * @return bool
+     */
+    public static function select(&$read, &$write, &$except, $tvSec, $tvUSec = 0);
+
+    /**
      * Read data from the stream.
      * 
      * @param int $length The length to read
