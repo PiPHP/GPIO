d7ab35ed199bb17ea1caf57414ddb7e30e9940cb
Andrew Carter <andrewcarter1992@gmail.com>
2016-03-31 00:31:11 +0100

    Update InterruptWatcherFactory.php

--- src/InterruptWatcherFactory.php
+++ src/InterruptWatcherFactory.php
@@ -2,13 +2,28 @@
 
 namespace PiPHP\GPIO;
 
+use PiPHP\GPIO\FileSystem\FileSystem;
+use PiPHP\GPIO\FileSystem\FileSystemInterface;
+
 final class InterruptWatcherFactory implements InterruptWatcherFactoryInterface
 {
+    private $fileSystem;
+
+    /**
+     * Constructor.
+     * 
+     * @param FileSystemInterface $fileSystem Optional file system object to use
+     */
+    public function __construct(FileSystemInterface $fileSystem = null)
+    {
+        $this->fileSystem = $fileSystem ?: new FileSystem();
+    }
+
     /**
      * {@inheritdoc}
      */
     public function createWatcher()
     {
-        return new InterruptWatcher();
+        return new InterruptWatcher($this->fileSystem);
     }
 }
