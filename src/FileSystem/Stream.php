be51d069aa3e91558d52a516948d3b2c14c7f269
Andrew Carter <andrewcarter1992@gmail.com>
2016-03-31 00:25:10 +0100

    Update Stream.php

--- src/FileSystem/Stream.php
+++ src/FileSystem/Stream.php
@@ -19,6 +19,32 @@ final class Stream implements StreamInterface
     /**
      * {@inheritdoc}
      */
+    public static function select(&$read, &$write, &$except, $tvSec, $tvUSec = 0)
+    {
+        $map = ['resources' => [], 'objects' => []];
+
+        foreach (['read' => $read, 'write' => $write, 'except' => $except] as $key => $streamObjects) {
+            $resources[$key] = $objects[$key] = [];
+
+            foreach ($streamObjects as $streamObject) {
+                $hash = spl_object_hash($streamObject);
+                $map['resources'][$key][$hash] = $streamObject->stream;
+                $map['objects'][$key][$hash] = $streamObject;
+            }
+        }
+
+        $result = stream_select($map['resources']['read'], $map['resources']['write'], $map['resources']['except'], $tvSec, $tvUSec);
+
+        $read = array_values(array_intersect_key($map['objects']['read'], $map['resources']['read']));
+        $write = array_values(array_intersect_key($map['objects']['write'], $map['resources']['write']));
+        $except = array_values(array_intersect_key($map['objects']['except'], $map['resources']['except']));
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function read($length)
     {
         return fread($this->stream, $length);
