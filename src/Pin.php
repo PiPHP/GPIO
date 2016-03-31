c0a40505cf6404eaabd989b5c96e5225eebfca7f
Andrew Carter <andrewcarter1992@gmail.com>
2016-03-31 01:11:02 +0100

    Update Pin.php

--- src/Pin.php
+++ src/Pin.php
@@ -1,27 +1,20 @@
 <?php
-
 namespace PiPHP\GPIO;
-
 use PiPHP\GPIO\FileSystem\FileSystemInterface;
-
 final class Pin implements PinInterface
 {
     // Paths
     const GPIO_PATH      = '/sys/class/gpio/';
     const GPIO_PREFIX    = 'gpio';
-
     // Files
     const GPIO_FILE_EXPORT   = 'export';
     const GPIO_FILE_UNEXPORT = 'unexport';
-
     // Pin files
     const GPIO_PIN_FILE_DIRECTION = 'direction';
     const GPIO_PIN_FILE_VALUE     = 'value';
     const GPIO_PIN_FILE_EDGE      = 'edge';
-
     private $fileSystem;
     private $number;
-
     /**
      * Constructor.
      * 
@@ -33,7 +26,6 @@ final class Pin implements PinInterface
         $this->fileSystem = $fileSystem;
         $this->number = $number;
     }
-
     /**
      * {@inheritdoc}
      */
@@ -41,7 +33,6 @@ final class Pin implements PinInterface
     {
         return $this->number;
     }
-
     /**
      * {@inheritdoc}
      */
@@ -49,7 +40,6 @@ final class Pin implements PinInterface
     {
         $this->writePinNumberToFile($this->getFile(self::GPIO_FILE_EXPORT));
     }
-
     /**
      * {@inheritdoc}
      */
@@ -57,61 +47,54 @@ final class Pin implements PinInterface
     {
         $this->writePinNumberToFile($this->getFile(self::GPIO_FILE_UNEXPORT));
     }
-
     /**
      * {@inheritdoc}
      */
     public function getDirection()
     {
         $directionFile = $this->getPinFile(self::GPIO_PIN_FILE_DIRECTION);
-        return $this->readFromFile($directionFile);
+        return $this->fileSystem->getContents($directionFile);
     }
-
     /**
      * {@inheritdoc}
      */
     public function setDirection($direction)
     {
         $directionFile = $this->getPinFile(self::GPIO_PIN_FILE_DIRECTION);
-        $this->writeToFile($directionFile, $direction);
+        $this->fileSystem->putContents($directionFile, $direction);
     }
-
     /**
      * {@inheritdoc}
      */
     public function getValue()
     {
         $valueFile = $this->getPinFile(self::GPIO_PIN_FILE_VALUE);
-        return (int) $this->readFromFile($valueFile);
+        return (int) $this->fileSystem->getContents($valueFile);
     }
-
     /**
      * {@inheritdoc}
      */
     public function setValue($value)
     {
         $valueFile = $this->getPinFile(self::GPIO_PIN_FILE_VALUE);
-        $this->writeToFile($valueFile, $value);
+        $this->fileSystem->putContents($valueFile, $value);
     }
-
     /**
      * {@inheritdoc}
      */
     public function getEdge()
     {
         $edgeFile = $this->getPinFile(self::GPIO_PIN_FILE_EDGE);
-        return $this->readFromFile($edgeFile);
+        return $this->fileSystem->getContents($edgeFile);
     }
-
     /**
      * {@inheritdoc}
      */
     public function setEdge($edge)
     {
         $edgeFile = $this->getPinFile(self::GPIO_PIN_FILE_EDGE);
-        $this->writeToFile($edgeFile, $edge);
+        $this->fileSystem->putContents($edgeFile, $edge);
     }
-
     /**
      * Get the path of the import or export file.
      * 
@@ -123,7 +106,6 @@ final class Pin implements PinInterface
     {
         return self::GPIO_PATH . $file;
     }
-
     /**
      * Get the path of a pin access file.
      * 
@@ -135,7 +117,6 @@ final class Pin implements PinInterface
     {
         return self::GPIO_PATH . self::GPIO_PREFIX . $this->getNumber() . '/' . $file;
     }
-
     /**
      * Write the pin number to a file.
      * 
@@ -143,35 +124,6 @@ final class Pin implements PinInterface
      */
     private function writePinNumberToFile($file)
     {
-        $this->writeToFile($file, $this->getNumber());
-    }
-
-    /**
-     * Write a value to a file.
-     * 
-     * @param string $file  The file to write to
-     * @param string $value The value to write
-     */
-    private function writeToFile($file, $value)
-    {
-        $stream = $this->fileSystem->open($file, "w");
-        $stream->write($value);
-        $stream->close();
-    }
-
-    /**
-     * Read a value from a file.
-     * 
-     * @param string $file The file to read from
-     * 
-     * @return string The value read
-     */
-    private function readFromFile($file)
-    {
-        $stream = $this->fileSystem->open($file, "r");
-        $result = $stream->read(1024);
-        $stream->close();
-
-        return trim($result);
+        $this->fileSystem->putContents($file, $this->getNumber());
     }
 }
