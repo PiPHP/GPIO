ea4e2b6f5f1499c922e54eca2ddd497907af3e1c
Andrew Carter <andrewcarter1992@gmail.com>
2016-03-31 01:15:19 +0100

    Update FileSystemTest.php

--- test/FileSystem/FileSystemTest.php
+++ test/FileSystem/FileSystemTest.php
@@ -3,7 +3,6 @@
 namespace PiPHP\Test\GPIO\FileSystem;
 
 use PiPHP\GPIO\FileSystem\FileSystem;
-use PiPHP\GPIO\FileSystem\StreamInterface;
 
 class FileSystemTest extends \PHPUnit_Framework_TestCase
 {
